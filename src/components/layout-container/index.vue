f71cc498bf46115028b6d627f3deab59544fd17c
wangwei <wangwei@ms-battery.cn>
2023-11-07 16:12:19 +0800

    feat 拖拽嵌套

--- src/components/layout-container/index.vue
+++ src/components/layout-container/index.vue
@@ -8,17 +8,7 @@
       </ul>
     </el-col>
     <el-col :span="20" class="container-item">
-      <div
-        class="ms-layout-container"
-        @dragover="dragover"
-        @dragleave="dragleave"
-      >
-        <!-- <div class="columns">
-          <div class="layoutItem" v-for="item in layout">
-            <b>{{ item.i }}</b
-            >: [{{ item.x }}, {{ item.y }}, {{ item.w }}, {{ item.h }}]
-          </div>
-        </div> -->
+      <div class="ms-layout-container" @dragover="dragover">
         <grid-layout
           v-drop="dragend"
           id="content"
@@ -44,19 +34,29 @@
             :i="item.i"
             :key="item.i"
           >
-            <template v-if="item.i == 0">
+            <template v-if="item.children && item.children.length">
               <LayoutChild
-                id="content-child"
-                ref="gridlayoutChildContainer"
-                :layout="childLayout"
-                v-drop="dragendChild"
-                @dragover.native="dragoverChild"
-                @dragleave="dragleaveChild"
+                :currRef="'gridlayoutChild' + item.i"
+                :layout="item.children"
+                :time="time"
+                :hoverParentStatus="hoverParentStatus"
+                @dragover.native="childDragover"
               ></LayoutChild>
             </template>
             <template v-else>
               <el-button type="success" size="mini">{{ index }}</el-button>
             </template>
+          </grid-item>
+          <grid-item
+            class="grid-item-placeholder"
+            v-if="Object.keys(currentNodeInfo).length"
+            :x="currentNodeInfo.x"
+            :y="currentNodeInfo.y"
+            :w="currentNodeInfo.w"
+            :h="currentNodeInfo.h"
+            :i="currentNodeInfo.i"
+            :key="currentNodeInfo.i"
+          >
           </grid-item>
         </grid-layout>
       </div>
@@ -78,44 +78,48 @@ export default {
       mouseXY: { x: null, y: null },
       DragPos: { x: null, y: null, w: 1, h: 1, i: null },
       layout: [
-        { x: 0, y: 0, w: 6, h: 25, i: "0" },
+        {
+          x: 0,
+          y: 0,
+          w: 6,
+          h: 25,
+          i: "0",
+          children: [
+            { x: 0, y: 0, w: 12, h: 4, i: "0" },
+            { x: 0, y: 5, w: 6, h: 4, i: "1" },
+            { x: 6, y: 5, w: 6, h: 4, i: "2" },
+            { x: 0, y: 9, w: 7, h: 4, i: "3" },
+            { x: 6, y: 9, w: 6, h: 4, i: "4" },
+            { x: 0, y: 13, w: 12, h: 4, i: "5" },
+          ],
+        },
         { x: 6, y: 0, w: 2, h: 4, i: "1" },
-        { x: 8, y: 0, w: 2, h: 5, i: "2" },
-        { x: 10, y: 0, w: 2, h: 3, i: "3" },
-        { x: 12, y: 0, w: 2, h: 3, i: "4" },
-        { x: 14, y: 0, w: 2, h: 3, i: "5" },
-        { x: 16, y: 5, w: 2, h: 5, i: "6" },
-        { x: 18, y: 5, w: 2, h: 5, i: "7" },
-        { x: 20, y: 5, w: 2, h: 5, i: "8" },
-        { x: 22, y: 3, w: 2, h: 4, i: "9" },
-        { x: 6, y: 3, w: 2, h: 4, i: "10" },
-        { x: 6, y: 4, w: 2, h: 4, i: "11" },
-        { x: 6, y: 10, w: 2, h: 5, i: "12" },
-        { x: 8, y: 10, w: 2, h: 5, i: "13" },
-        { x: 10, y: 8, w: 2, h: 4, i: "14" },
-        { x: 12, y: 8, w: 2, h: 4, i: "15" },
-        { x: 14, y: 10, w: 2, h: 5, i: "16" },
-        { x: 10, y: 6, w: 6, h: 2, i: "17" },
-        { x: 16, y: 9, w: 4, h: 3, i: "18" },
-        { x: 20, y: 6, w: 2, h: 2, i: "19" },
-      ],
-      childLayout: [
-        { x: 0, y: 0, w: 12, h: 4, i: "0" },
-        { x: 0, y: 5, w: 6, h: 4, i: "1" },
-        { x: 6, y: 5, w: 6, h: 4, i: "2" },
-        { x: 0, y: 9, w: 6, h: 4, i: "3" },
-        { x: 6, y: 9, w: 6, h: 4, i: "4" },
-        { x: 0, y: 13, w: 12, h: 4, i: "5" },
+        {
+          x: 8,
+          y: 0,
+          w: 6,
+          h: 20,
+          i: "2",
+          children: [
+            { x: 0, y: 0, w: 12, h: 4, i: "0" },
+            { x: 0, y: 9, w: 7, h: 4, i: "3" },
+            { x: 6, y: 9, w: 6, h: 4, i: "4" },
+            { x: 0, y: 13, w: 12, h: 4, i: "5" },
+          ],
+        },
       ],
       dragItem: {},
       dragChildItem: {},
       // 时间戳
-      time: 0,
+      time: 0, // 当前组件的唯一属性
+      // 当前拖拽元素信息
+      currentNodeInfo: {},
+      // placeholderArr
+      placeholderArr: [],
       // 父级容器元素信息
       parentRect: {},
-      childRect: {},
-      // 鼠标偏移量
-      cursorOffset: { x: 0, y: 0 },
+      // 是否在父元素上
+      hoverParentStatus: true,
     };
   },
   mounted() {},
@@ -123,100 +127,46 @@ export default {
     dragStart(e) {
       console.log(e);
       this.time = new Date().getTime();
-      // 记录鼠标拖拽时，鼠标距离当前拖拽元素左上角顶点的距离
-      this.cursorOffset = {
-        x: e.offsetX,
-        y: e.offsetY,
+      this.currentNodeInfo = {
+        x: (this.layout.length * 2) % (this.colNum || 24),
+        y: this.layout.length + (this.colNum || 24), // puts it at the bottom
+        w: 2,
+        h: 2,
+        i: "drop" + this.time,
       };
       this.parentRect = document
         .getElementById("content")
-        .getBoundingClientRect();
-      this.childRect = document
-        .getElementById("content-child")
-        .getBoundingClientRect();
+        ?.getBoundingClientRect();
     },
     dragover(e) {
       e.stopPropagation();
+      this.hoverParentStatus = true;
       // console.log(e);
-      this.mouseXY.x = e.clientX - this.cursorOffset.x;
-      this.mouseXY.y = e.clientY - this.cursorOffset.y;
-      let mouseInGrid = false;
-      if (
-        this.mouseXY.x > this.parentRect.left &&
-        this.mouseXY.x < this.parentRect.right &&
-        this.mouseXY.y > this.parentRect.top &&
-        this.mouseXY.y < this.parentRect.bottom
-      ) {
-        mouseInGrid = true;
-      }
-      if (
-        mouseInGrid === true &&
-        this.layout.findIndex((item) => item.i === "drop" + this.time) === -1
-      ) {
-        this.dragItem = {
-          x: (this.layout.length * 2) % (this.colNum || 24),
-          y: this.layout.length + (this.colNum || 24), // puts it at the bottom
-          w: 2,
-          h: 2,
-          i: "drop" + this.time,
-        };
-        this.layout.push(this.dragItem);
-      }
-      if (!mouseInGrid) {
-        this.throttle(() => {
-          const index = this.layout.indexOf(this.dragItem);
-          if (index > -1) {
-            this.layout.splice(index, 1);
-          }
-        }, 200);
-      }
-      // console.log(this.layout)
-      let index = this.layout.findIndex(
-        (item) => item.i === "drop" + this.time
+      // 以被拖拽元素左上角定点位置为基准，计算当前组件应该放置的位置
+      this.mouseXY.x = e.clientX;
+      this.mouseXY.y = e.clientY;
+
+      let el = this.$refs.gridlayout.$children.at(-1);
+      // console.log(this.$refs.gridlayout);
+      el.dragging = {
+        top: this.mouseXY.y - this.parentRect.top,
+        left: this.mouseXY.x - this.parentRect.left,
+      };
+      let new_pos = el.calcXY(
+        this.mouseXY.y - this.parentRect.top,
+        this.mouseXY.x - this.parentRect.left
       );
-      // console.log(index);
-      if (index !== -1) {
-        let el = this.$refs.gridlayout.$children[index];
-        // console.log(this.$refs.gridlayout);
-        el.dragging = {
-          top: this.mouseXY.y - this.parentRect.top,
-          left: this.mouseXY.x - this.parentRect.left,
-        };
-        let new_pos = el.calcXY(
-          this.mouseXY.y - this.parentRect.top,
-          this.mouseXY.x - this.parentRect.left
-        );
-        // console.log(new_pos);
-        if (mouseInGrid === true) {
-          this.$refs.gridlayout.dragEvent(
-            "dragstart",
-            "drop" + this.time,
-            new_pos.x,
-            new_pos.y,
-            2,
-            2
-          );
-          this.DragPos.i = String(index);
-          this.DragPos.x = this.layout[index].x;
-          this.DragPos.y = this.layout[index].y;
-        }
-        if (mouseInGrid === false) {
-          this.$refs.gridlayout.dragEvent(
-            "dragend",
-            "drop" + this.time,
-            new_pos.x,
-            new_pos.y,
-            2,
-            2
-          );
-          this.layout = this.layout.filter(
-            (obj) => obj.i !== "drop" + this.time
-          );
-        }
-      }
+      this.currentNodeInfo = {
+        x: new_pos.x,
+        y: new_pos.y, // puts it at the bottom
+        w: 2,
+        h: 2,
+        i: "drop" + this.time,
+      };
     },
     dragend(e) {
       console.log(e);
+      // 拖拽结束，根据当前位置，防止对应元素
       let mouseInGrid = false;
       if (
         this.mouseXY.x > this.parentRect.left &&
@@ -226,161 +176,21 @@ export default {
       ) {
         mouseInGrid = true;
       }
-      console.log(mouseInGrid);
-      if (mouseInGrid === true) {
-        this.$refs.gridlayout.dragEvent(
-          "dragend",
-          "drop" + this.time,
-          this.DragPos.x,
-          this.DragPos.y,
-          2,
-          2
-        );
-      }
-    },
-    dragleave(e) {
-      e.stopPropagation();
-      // console.log(e.target.classList)
-      // if(e.target.classList && Array.from(e.target.classList).indexOf('ms-grid-layout-container') > -1) {
-      //   const index = this.layout.indexOf(this.dragItem)
-      //   if(index > -1){
-      //     this.layout.splice(index, 1)
-      //   }
-      //   console.log(this.layout.indexOf(this.dragItem))
-      // }
-    },
-    dragoverChild(e) {
-      e.stopPropagation();
-      // console.log(e);
-      this.mouseXY.x = e.clientX - this.cursorOffset.x;
-      this.mouseXY.y = e.clientY - this.cursorOffset.y;
-      let mouseInGrid = false;
-      if (
-        this.mouseXY.x > this.childRect.left &&
-        this.mouseXY.x < this.childRect.right &&
-        this.mouseXY.y > this.childRect.top &&
-        this.mouseXY.y < this.childRect.bottom
-      ) {
-        mouseInGrid = true;
-      }
       if (
         mouseInGrid === true &&
-        this.childLayout.findIndex((item) => item.i === "drop" + this.time) ===
-          -1
+        this.layout.findIndex((item) => item.i === "drop" + this.time) === -1 &&
+        Object.keys(this.currentNodeInfo).length
       ) {
-        this.dragChildItem = {
-          x: (this.childLayout.length * 2) % (this.colNum || 24),
-          y: this.childLayout.length + (this.colNum || 24), // puts it at the bottom
-          w: 6,
-          h: 2,
-          i: "drop" + this.time,
-        };
-        this.childLayout.push(this.dragChildItem);
-      }
-      if (!mouseInGrid) {
-        this.throttle(() => {
-          const index = this.childLayout.indexOf(this.dragChildItem);
-          if (index > -1) {
-            this.childLayout.splice(index, 1);
-          }
-        }, 200);
-      }
-      // console.log(this.layout)
-      let index = this.childLayout.findIndex(
-        (item) => item.i === "drop" + this.time
-      );
-      // console.log(index);
-      if (index !== -1) {
-        let el =
-          this.$refs.gridlayoutChildContainer[0].$refs.gridlayoutChild
-            .$children[index];
-        // console.log(this.$refs.gridlayout);
-        el.dragging = {
-          top: this.mouseXY.y - this.childRect.top,
-          left: this.mouseXY.x - this.childRect.left,
-        };
-        let new_pos = el.calcXY(
-          this.mouseXY.y - this.childRect.top,
-          this.mouseXY.x - this.childRect.left
-        );
-        // console.log(new_pos);
-        if (mouseInGrid === true) {
-          this.$refs.gridlayoutChildContainer[0].$refs.gridlayoutChild.dragEvent(
-            "dragstart",
-            "drop" + this.time,
-            new_pos.x,
-            new_pos.y,
-            6,
-            2
-          );
-          this.DragPos.i = String(index);
-          this.DragPos.x = this.layout[index].x;
-          this.DragPos.y = this.layout[index].y;
-        }
-        if (mouseInGrid === false) {
-          this.$refs.gridlayoutChildContainer[0].$refs.gridlayoutChild.dragEvent(
-            "dragend",
-            "drop" + this.time,
-            new_pos.x,
-            new_pos.y,
-            6,
-            2
-          );
-          this.childLayout = this.childLayout.filter(
-            (obj) => obj.i !== "drop" + this.time
-          );
-        }
+        this.layout.push(this.currentNodeInfo);
+        this.currentNodeInfo = {};
       }
     },
-    dragendChild(e) {
-      e.stopPropagation();
-      console.log(e);
-      let mouseInGrid = false;
-      if (
-        this.mouseXY.x > this.childRect.left &&
-        this.mouseXY.x < this.childRect.right &&
-        this.mouseXY.y > this.childRect.top &&
-        this.mouseXY.y < this.childRect.bottom
-      ) {
-        mouseInGrid = true;
-      }
-      console.log(mouseInGrid);
-      if (mouseInGrid === true) {
-        this.$refs.gridlayoutChildContainer[0].$refs.gridlayoutChild.dragEvent(
-          "dragend",
-          "drop" + this.time,
-          this.DragPos.x,
-          this.DragPos.y,
-          6,
-          2
-        );
-      }
-    },
-    dragleaveChild(e) {
-      // e.stopPropagation()
-      // if(e.target.classList && Array.from(e.target.classList).indexOf('ms-grid-layout-child') > -1) {
-      //   const index = this.childLayout.indexOf(this.dragChildItem)
-      //   if(index > -1){
-      //     this.childLayout.splice(index, 1)
-      //   }
-      //   console.log(this.childLayout.indexOf(this.dragChildItem))
-      // }
+    childDragover() {
+      this.currentNodeInfo = {};
+      this.hoverParentStatus = false;
     },
     layoutUpdatedEvent(layout) {
       // console.log(JSON.stringify(layout));
-    },
-    throttle(fn, delay) {
-      let timer = null;
-      return function () {
-        let context = this;
-        let args = arguments;
-        if (!timer) {
-          timer = setTimeout(function () {
-            fn.apply(context, args);
-            timer = null;
-          }, delay);
-        }
-      };
     },
   },
 };
@@ -416,11 +226,14 @@ export default {
     }
   }
 }
+.container-item {
+  height: 100%;
+}
 .ms-layout-container {
   height: 100%;
   width: 100%;
   ::v-deep .ms-grid-layout-container {
-    height: 100%;
+    height: 100% !important;
     .vue-grid-item {
       display: flex;
       align-items: center;
--- src/components/layout-container/layout-child.vue
+++ src/components/layout-container/layout-child.vue
@@ -1,16 +1,14 @@
 <template>
-  <div class="ms-layout-container ms-layout-container-child">
-    <!-- <div class="columns">
-      <div class="layoutItem" v-for="item in layout">
-        <b>{{ item.i }}</b
-        >: [{{ item.x }}, {{ item.y }}, {{ item.w }}, {{ item.h }}]
-      </div>
-    </div> -->
+  <div
+    class="ms-layout-container ms-layout-container-child"
+    id="child-grid-container"
+    v-drop="dragendChild"
+    @dragover="dragoverChild"
+  >
     <grid-layout
-      id="content"
-      ref="gridlayoutChild"
+      :ref="currRef"
       class="ms-grid-layout-child"
-      :layout.sync="layout"
+      :layout.sync="childLayout"
       :col-num="12"
       :row-height="10"
       :is-draggable="true"
@@ -31,6 +29,17 @@
       >
         <el-button size="mini">{{ index }}</el-button>
       </grid-item>
+      <grid-item
+        class="grid-item-placeholder"
+        v-if="Object.keys(currentNodeInfo).length"
+        :x="currentNodeInfo.x"
+        :y="currentNodeInfo.y"
+        :w="currentNodeInfo.w"
+        :h="currentNodeInfo.h"
+        :i="currentNodeInfo.i"
+        :key="currentNodeInfo.i"
+      >
+      </grid-item>
     </grid-layout>
   </div>
 </template>
@@ -46,22 +55,127 @@ export default {
   props: {
     layout: {
       type: Array,
-      default: () => []
-    }
+      default: () => [],
+    },
+    time: {
+      type: [String, Number],
+      default: 0,
+    },
+    hoverParentStatus: {
+      type: Boolean,
+      default: true,
+    },
+    currRef: {
+      type: [String, Number],
+      default: '',
+    },
   },
   data() {
     return {
       mouseXY: { x: null, y: null },
       DragPos: { x: null, y: null, w: 1, h: 1, i: null },
-      currentNode: [],
+      currentNodeInfo: {},
+      childLayout: [],
+      containerRect: null,
     };
   },
+  watch: {
+    layout: {
+      immediate: true,
+      // deep: true,
+      handler(val) {
+        console.log(val);
+        this.childLayout = val;
+      },
+    },
+    hoverParentStatus: {
+      immediate: true,
+      // deep: true,
+      handler(val) {
+        if (val) {
+          console.log("hoverParentStatus", val);
+          this.currentNodeInfo = {};
+        }
+      },
+    },
+  },
   mounted() {
+    console.log(this.$refs.gridlayoutChild);
+    setTimeout(() => {
+      this.containerRect =
+        this.$refs[this.currRef].$el?.getBoundingClientRect();
+      console.log("this.containerRect.left", this.containerRect.left);
+      console.log("this.containerRect.right", this.containerRect.right);
+      console.log("this.containerRect.top", this.containerRect.top);
+      console.log("this.containerRect.bottom", this.containerRect.bottom);
+      console.log("---------------------------------------------------");
+    });
+    setTimeout(() => {
+      this.containerRect =
+        this.$refs[this.currRef].$el?.getBoundingClientRect();
+      console.log("this.containerRect.left", this.containerRect.left);
+      console.log("this.containerRect.right", this.containerRect.right);
+      console.log("this.containerRect.top", this.containerRect.top);
+      console.log("this.containerRect.bottom", this.containerRect.bottom);
+      console.log("---------------------------------------------------");
+    }, 1000);
   },
   methods: {
     layoutUpdatedEvent() {
       // console.log(JSON.stringify(this.layout));
-    }
+    },
+    dragoverChild(e) {
+      e.stopPropagation();
+      // console.log(e);
+      // 以被拖拽元素左上角定点位置为基准，计算当前组件应该放置的位置
+      this.mouseXY.x = e.clientX;
+      this.mouseXY.y = e.clientY;
+
+      let el = this.$refs[this.currRef]?.$children.at(-1);
+      // console.log(el);
+      el.dragging = {
+        top: this.mouseXY.y - this.containerRect.top,
+        left: this.mouseXY.x - this.containerRect.left,
+      };
+      let new_pos = el.calcXY(
+        this.mouseXY.y - this.containerRect.top,
+        this.mouseXY.x - this.containerRect.left
+      );
+      this.currentNodeInfo = {
+        x: new_pos.x,
+        y: new_pos.y, // puts it at the bottom
+        w: 2,
+        h: 2,
+        i: "drop" + this.time,
+      };
+    },
+    dragendChild(e) {
+      e.stopPropagation();
+      // 拖拽结束，根据当前位置，防止对应元素
+      let mouseInGrid = false;
+      if (
+        this.mouseXY.x > this.containerRect.left &&
+        this.mouseXY.x < this.containerRect.right &&
+        this.mouseXY.y > this.containerRect.top &&
+        this.mouseXY.y < this.containerRect.bottom
+      ) {
+        mouseInGrid = true;
+      }
+      console.log("this.mouseXY.x", this.mouseXY.x);
+      console.log("this.mouseXY.x", this.mouseXY.y);
+      console.log(mouseInGrid);
+      console.log(this.currentNodeInfo);
+      if (
+        mouseInGrid === true &&
+        this.childLayout.findIndex((item) => item.i === "drop" + this.time) ===
+          -1 &&
+        Object.keys(this.currentNodeInfo).length
+      ) {
+        this.childLayout.push(this.currentNodeInfo);
+        this.currentNodeInfo = {};
+      }
+      console.log(JSON.stringify(this.childLayout))
+    },
   },
 };
 </script>
@@ -70,7 +184,7 @@ export default {
   height: 100%;
   width: 100%;
   ::v-deep .ms-grid-layout-child {
-    height: 100%;
+    height: 100% !important;
     .vue-grid-item {
       background: #009588;
     }
